e15e1a67621a7e4c8fc9460354d780a92f3637f8
Martin Arroyo <ma.arroyoq@duocuc.cl>
2024-10-09 20:08:50 -0300

    Uso del metodo GET

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,12 +1,30 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ApiService } from '../services/api.service';
 
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
 })
-export class HomePage {
+export class HomePage implements OnInit {
 
-  constructor() {}
+  posts: any[] = [];
 
+  constructor(
+    private apiService: ApiService
+  ) {}
+
+  ngOnInit() {
+    this.loadPosts();
+  }
+
+  loadPosts() {
+    this.apiService.getData().subscribe((data: any) => {
+      this.posts = data;
+      console.log('Datos cargados:', this.posts);
+    }, (error) => {
+      console.error('Error al cargar los datos:', error);
+    }
+  );
+  }
 }
